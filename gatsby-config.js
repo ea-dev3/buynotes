27f5af276954d7ecb8cd7327f39f2b799abfb976
swyx <swyx@dontemail.me@>
2018-11-06 13:57:18 -0800

    rename

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify Functions + Netlify Identity',
+    title: 'JAMstack Hackathon Starter',
   },
 
   // for avoiding CORS while developing Netlify Functions locally
